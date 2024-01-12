7b66d2be07ecd3935e79640bac18f26a5f8b6827
Vegar Karlsen <vegar@live.no>
2024-01-12 17:43:15 +0100

    Restructure repo.
    Thinking that the project, and exercises can live in different branches.

---
+++ exercises/exercise_1/shared variable/go/foo.go
@@ -0,0 +1,48 @@
+// Use `go run foo.go` to run your program
+
+package main
+
+import (
+    . "fmt"
+    "runtime"
+    "time"
+)
+
+var i = 0;
+
+// var inc, dec, get chan int;
+
+
+
+func incrementing() {
+    //TODO: increment i 1000000 times
+    for a := 0; a < 1000000; a++{
+        Println("dec");
+        i++; 
+    }
+
+}
+
+func decrementing() {
+    //TODO: decrement i 1000000 times
+    for a := 0; a < 1000000; a++ {
+        Println("dec");
+        i--;
+    }
+}
+
+func main() {
+    // What does GOMAXPROCS do? What happens if you set it to 1?
+    // Maximum threads that can run, if we change to on then one routine will run first and then the other afterwords. instead of "simoultanizaly"
+    runtime.GOMAXPROCS(2)    
+	
+    // TODO: Spawn both functions as goroutines
+    go incrementing();
+    go decrementing();
+
+	
+    // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
+    // We will do it properly with channels soon. For now: Sleep.
+    time.Sleep(500*time.Millisecond)
+    Println("The magic number is:", i)
+}
