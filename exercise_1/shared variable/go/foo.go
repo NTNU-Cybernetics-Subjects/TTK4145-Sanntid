dfa8e6873df0acd2a6f0868f8b7d577220d84f63
Vegar Karlsen <vegar@live.no>
2024-01-16 13:42:57 +0100

    Task 4 done.
    Implemented shared variable synchronization in go.

--- exercise_1/shared variable/go/foo.go
+++ exercise_1/shared variable/go/foo.go
@@ -10,35 +10,62 @@ import (
 
 var i = 0
 
-func incrementing() {
+
+func incrementing(inc chan bool, finished chan bool) {
     //TODO: increment i 1000000 times
     for a := 0; a < 1000000; a++{
-        Println("dec");
-        i++; 
+        // Println("dec");
+        // i++; 
+        inc <- true
     }
+    finished <- true
 
 }
 
-func decrementing() {
+func decrementing(dec chan bool, finished chan bool) {
     //TODO: decrement i 1000000 times
-    for a := 0; a < 1000000; a++ {
-        Println("dec");
-        i--;
+    for a := 0; a < 999999; a++ {
+        // Println("dec");
+        // i--;
+        dec <- true
     }
+    finished <- true
 }
+
 
 func main() {
     // What does GOMAXPROCS do? What happens if you set it to 1?
     // Maximum threads that can run, if we change to on then one routine will run first and then the other afterwords. instead of "simoultanizaly"
     runtime.GOMAXPROCS(2)    
-	
-    // TODO: Spawn both functions as goroutines
-    go incrementing();
-    go decrementing();
+    // get := make(chan int)
+    inc := make(chan bool)
+    dec := make(chan bool)
+    finished := make(chan bool)
 
+    // TODO: Spawn both functions as goroutines
+    go incrementing(inc, finished);
+    go decrementing(dec, finished);
+
+    warriors := 0
+
+    for ; warriors < 2 ;{
+        select{
+        case <- inc:
+            i++
+            // Println("inc")
+        case <- dec:
+            i--
+            // Println("dec")
+        case <- finished:
+            warriors++ 
+        }
+    }
 	
+    // Println(new_test)
     // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
     // We will do it properly with channels soon. For now: Sleep.
     time.Sleep(500*time.Millisecond)
     Println("The magic number is:", i)
+
+
 }
