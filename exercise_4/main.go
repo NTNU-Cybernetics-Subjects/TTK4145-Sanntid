32f5e51d914fdde88f3408565375daab551599d6
Vegar Karlsen <vegar@live.no>
2024-02-11 22:06:42 +0100

    Rename do-shell to DoShell, and fix some comments.

--- exercise_4/main.go
+++ exercise_4/main.go
@@ -11,7 +11,7 @@ import (
     "os/exec"
 )
 
-const PORT int = 16569
+const PORT int = 1234
 
 type Message struct {
     Id int;
@@ -37,10 +37,9 @@ func Backup(id int, terminateValue* int){
         default:
             // TODO: need a faster way of detecting connection loss
             if (time.Now().UnixMilli() - lastRevicedPackage) > 700{
-                // send takeover signal to main
                 fmt.Println("No connection")
                 *terminateValue = count
-                // fmt.Println("Count sendt")
+                // return to stop backup mode and takeover Primary
                 return
                 
             }
@@ -54,7 +53,7 @@ func Primary(id int, start_count int){
     fmt.Println("takeover as Primary")
 
     // open new backup
-    Do_shell("go run ~/school/sanntid/exercise_4/main.go -backup 1")
+    DoShell("go run ~/school/sanntid/exercise_4/main.go -backup 1")
 
 
 	varTx := make(chan Message)
@@ -73,7 +72,7 @@ func Primary(id int, start_count int){
 
 }
 
-func Do_shell(command string){
+func DoShell(command string){
  
     cmd := exec.Command("gnome-terminal", "--", "bash", "-c", command)
 
@@ -98,6 +97,7 @@ func main(){
 
     var terminateValue int
     start_count := 0
+    id := 0
 
     if backup != -1{
         Backup(1, &terminateValue)
@@ -107,6 +107,6 @@ func main(){
     }
 
     // if not backup start Primary (Primary always starts a backup)
-    Primary(1, start_count)
+    Primary(id, start_count)
 
 }
