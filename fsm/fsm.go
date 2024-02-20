f9d917cc0f272dd08866b5c2801c61a7a947e9bc
Glenn Varhaug <glennrv@lyse.net>
2024-02-20 19:44:08 +0100

    Added IO overview

---
+++ fsm/fsm.go
@@ -0,0 +1,27 @@
+package fsm
+
+import(
+	"Driver-go/elevio"
+	"fmt"
+)
+
+/*
+Finite state machine for operating a single elevator.
+
+Inputs from:
+	Elevio:
+		- FloorSensor
+		- ObstructionSwitch
+		- StopButton
+	Distibutor:
+		- DecisionCommand - Not Defined Yet
+
+Outputs to:
+	Distibutor:
+		State: - Not Finalised
+			- Floor
+			- Obstruction
+			- Direction
+			- ServiceQueue
+'/
+
