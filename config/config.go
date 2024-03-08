e3f7aca8fa0fd5b05cdf6721e96df966c89c6513
Vegar Karlsen <vegar@live.no>
2024-03-08 18:25:23 +0100

    Add config file.

---
+++ config/config.go
@@ -0,0 +1,17 @@
+
+package config
+
+const (
+	PEER_PORT  int    = 12348
+	HOST       string = "localhost"
+	BCAST_PORT int    = 4875
+)
+
+const (
+	n_elevators int = 3
+	NumberFloors    int = 4
+)
+
+const (
+    BroadcastStateIntervalMs int64 = 800 // ms
+)
