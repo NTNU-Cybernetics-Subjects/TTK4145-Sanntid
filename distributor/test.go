bdaf74c6c4e198bd539a7ee64011fa89b77f56eb
Vegar Karlsen <vegar@live.no>
2024-03-11 17:33:16 +0100

    Add a function for making testElevators, and test Hallreqeusts.

---
+++ distributor/test.go
@@ -0,0 +1,45 @@
+package distribitor
+
+import (
+	"Driver-go/elevio"
+	"elevator/config"
+	"elevator/fsm"
+	"fmt"
+)
+
+// import "testing"
+
+
+func MakeTestElevators() ([]string){
+
+    activePeers := [2]string{"first", "second"}
+    addElevator(activePeers[0])
+    addElevator(activePeers[1])
+    
+    firstElevator := ElevatorState{
+        Behavior: fsm.EB_Moving,
+        Floor: 2,
+        Direction: elevio.MD_Up,
+        CabRequests: []bool{true, true, true, true},
+        Obstructed: false,
+    }
+    secondElevator := ElevatorState{
+        Behavior: fsm.EB_DoorOpen,
+        Floor: 2,
+        Direction: elevio.MD_Stop,
+        CabRequests: []bool{false,false,false,false},
+    }
+
+    var testHallRequests [config.NumberFloors][2]bool
+    // set two hall orders
+    testHallRequests[1][0] = true
+    testHallRequests[config.NumberFloors-1][1] = true
+    
+    fmt.Println("updating first elevator to: ", firstElevator)
+    updateElevator(activePeers[0], firstElevator)
+    fmt.Println("updating second elevator to: ", secondElevator)
+    updateElevator(activePeers[1], secondElevator)
+
+    updateHallRequests([config.NumberFloors][2]bool(testHallRequests))
+    return activePeers[:]
+}
