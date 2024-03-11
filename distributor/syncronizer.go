e1564642a5238a934d3f4a41c257ce0a5e533493
Vegar Karlsen <vegar@live.no>
2024-03-11 15:23:45 +0100

    Move requestHanlder out of syncronizer, and to its own file.
    This is done to have better overview over the code and what it does.

--- distributor/syncronizer.go
+++ distributor/syncronizer.go
@@ -1,21 +1,16 @@
 package distribitor
 
 import (
-	// "Driver-go/elevio"
-	"Driver-go/elevio"
 	"elevator/config"
-
-	// "Network-go/network/bcast"
 	"Network-go/network/peers"
-	"fmt"
+	// "fmt"
 	"log/slog"
 	"sync"
 	"time"
 )
 
 /*
-Syncronizer part of distributor. The purpose is to keep track of all the active
-peers in the network, and syncronize the state among them. This is done by broadcasting
+Syncronizer part of distributor. The purpose is to keep track of all the active peers in the network, and syncronize the state among them. This is done by broadcasting
 the local state of our elevator to the other peers, and update our local overview of the
 other peers's state that are broadcasted.
 * */
@@ -35,19 +30,20 @@ type StateMessageBroadcast struct {
 	State        State
 	Sequence     int
 	HallRequests [config.NumberFloors][2]bool
-	UpdateOrders bool
 }
+
 
 /* Local collection of all states and hall requests. */
 var (
 	localPeerStates   map[string]State = make(map[string]State)
-	localHallRequests [config.NumberFloors][2]bool
+    localPeerStateLock    sync.Mutex
 
-	localPeerStateLock    sync.Mutex
-	localHallRequestsLock sync.Mutex
+    localHallRequests [config.NumberFloors][2]bool
+    localHallRequestsLock sync.Mutex
 
-	messageSequnceNumber int
-	messageSequenceLock  sync.Mutex
+    activePeers []string
+    activePeersLock sync.Mutex
+
 )
 
 func addElevator(id string) {
@@ -105,143 +101,61 @@ func getHallReqeusts() [config.NumberFloors][2]bool {
 	return HallRequests
 }
 
-func waitForHallOrderConfirmation(
+func Syncronizer(
 	mainID string,
-	buttonEvent elevio.ButtonEvent,
-	activePeers []string,
-	ackChan <-chan string,
+	broadcastStateMessageTx chan<- StateMessageBroadcast,
+	broadcastStateMessageRx <-chan StateMessageBroadcast,
+	peerUpdatesRx <-chan peers.PeerUpdate,
 ) {
-	// FIXME: can use lenght of peerMap here since we dont bother about id.
-	acknowledgmentsNeeded := len(activePeers)
-	countAck := 0
-	startTime := time.Now().UnixMilli()
-
-	fmt.Println("[waitForConfirmation]: Required acks: ", acknowledgmentsNeeded)
-
-	for {
-		select {
-		case <-ackChan:
-			countAck += 1
-			fmt.Println("ack count: ", countAck) // FIXME:
-			if countAck >= acknowledgmentsNeeded {
-				// TODO: check that the direction is correct.
-				setHallRequest(buttonEvent.Floor, int(buttonEvent.Button), true)
-				fmt.Println("HallRequest granted: ", getHallReqeusts())
-				return
-
-			}
-		default:
-			if time.Now().UnixMilli() >= startTime+config.HallOrderAcknowledgeTimeOut {
-				// Timeout, we drop the request.
-				fmt.Println("[waitForConfirmation]: timed out")
-				return
-			}
-		}
-	}
-}
-
-func makeBroadcastStateMessage(mainID string) StateMessageBroadcast {
+	lastStateBroadcast := time.Now().UnixMilli() - config.BroadcastStateIntervalMs // To broadcast imideatly
 	stateMessage := StateMessageBroadcast{
 		Id:           mainID,
 		HallRequests: getHallReqeusts(),
 		State:        getElevatorState(mainID),
 		Sequence:     0,
 		Checksum:     nil,
-		UpdateOrders: false,
 	}
-	// Automatically increment sequence number when making new stateMessage
-	messageSequenceLock.Lock()
-	stateMessage.Sequence = messageSequnceNumber
-	messageSequnceNumber += 1
-	messageSequenceLock.Unlock()
-
-	stateMessage.Checksum, _ = HashStructSha1(stateMessage)
-	return stateMessage
-}
-
-func validAck(message StateMessageBroadcast) (bool){
-    return true
-}
-
-func Syncronizer(
-	mainID string,
-	broadcastStateMessageTx chan<- StateMessageBroadcast,
-	broadcastStateMessageRx <-chan StateMessageBroadcast,
-	peerUpdatesRx <-chan peers.PeerUpdate,
-	buttonEvent <-chan elevio.ButtonEvent,
-) {
-	lastStateBroadcast := time.Now().UnixMilli() - config.BroadcastStateIntervalMs // To broadcast imideatly
-	acknowledgeGranted := make(chan string)
-    // HallOrderConfirmationActive := false
-	var activePeers []string
-    
 
 	for {
 		select {
 		case peerUpdate := <-peerUpdatesRx:
-			activePeers = peerUpdate.Peers
+
+            activePeersLock.Lock()
+            activePeers = peerUpdate.Peers
+            activePeersLock.Unlock()
+
 			if peerUpdate.New != "" {
 				slog.Info("[peerUpdate]: Adding elevator", slog.String("ID", peerUpdate.New))
 				addElevator(peerUpdate.New)
 				// TODO: distribute
-
 			}
 
+            // FIXME: this should not remove, we want to store the state of other elevators if they reconnect.
 			for i := 0; i < len(peerUpdate.Lost); i++ {
 				slog.Info("[peerUpdate]: Removing elevator", slog.String("ID", peerUpdate.Lost[i]))
 				removeElevator(peerUpdate.Lost[i])
+                // TODO: distribute
 			}
 
-			// Syncroniz incoming state, or ack
+			// Syncronize incoming state
 		case incommingStateMessage := <-broadcastStateMessageRx:
-			// fmt.Println("[syncronizer]: updated elevator ", incommingStateMessage.Id, " to ", incommingStateMessage.State)
-			if !incommingStateMessage.UpdateOrders {
-				slog.Info("[Broadcast<-]: Sync", slog.String("elevator", incommingStateMessage.Id))
-				updateElevator(incommingStateMessage.Id, incommingStateMessage.State)
-				// If HallRequests is broadscasted without Update orders flag it is valid.
-				updateHallRequests(incommingStateMessage.HallRequests)
-				continue
-			}
-			slog.Info("[Broadcast<-]: updateOrders=", incommingStateMessage.UpdateOrders)
+            // fmt.Println(mainID, incommingStateMessage.HallRequests)
+            slog.Info("[Broadcast<-]: Syncing hallrequests to: ", incommingStateMessage.HallRequests)
+			updateElevator(incommingStateMessage.Id, incommingStateMessage.State)
+            // The broadcasted hallRequests are always valid. FIXME: check if this creates race condition when new request go through.
+			updateHallRequests(incommingStateMessage.HallRequests)
 
-            // Recived an ack
-			if incommingStateMessage.Id == mainID{
-                if !validAck(incommingStateMessage){
-                    continue
-                }
-				slog.Info("[Broadcast<-] Registred ack", slog.String("checksum", string(incommingStateMessage.Checksum)))
-                acknowledgeGranted <- mainID // FIXME: This deadlocks on the reciving end. (if waitForHallOrderConfirmation is not running)
-				continue
-			}
-			// we ack other elevators's request
-			incommingStateMessage.Id = mainID
-			incommingStateMessage.Checksum, _ = HashStructSha1(incommingStateMessage)
-			slog.Info("[Broadcast<-]: Acking message from", slog.String("ID", incommingStateMessage.Id), slog.String("Answer checksum", string(incommingStateMessage.Checksum)))
-			broadcastStateMessageTx <- incommingStateMessage
-
-			// assuming here we only get hall calls on this channel
-		case button := <-buttonEvent:
-			if button.Button == elevio.BT_Cab {
-				continue
-			}
-			fmt.Println("[ButtonPress]: floor", button.Floor, "direction", button.Button)
-			newStateMessageOrder := makeBroadcastStateMessage(mainID)
-			newStateMessageOrder.UpdateOrders = true
-			newStateMessageOrder.HallRequests[button.Floor][button.Button] = true
-			fmt.Println("[request] sending out hall request, ", newStateMessageOrder)
-
-			go waitForHallOrderConfirmation(mainID, button, activePeers, acknowledgeGranted)
-            broadcastStateMessageTx <- newStateMessageOrder
-
-			// broadcast state
 		default:
 			if time.Now().UnixMilli() < lastStateBroadcast+config.BroadcastStateIntervalMs {
 				continue
 			}
 
-			newStateMessage := makeBroadcastStateMessage(mainID)
+            stateMessage.State = getElevatorState(mainID)
+            stateMessage.HallRequests = getHallReqeusts()
+            stateMessage.Checksum,_ = HashStructSha1(stateMessage)
 			// fmt.Println("[syncronizer]: Broadcasting, Checksum: ", newStateMessage.Checksum)
-			broadcastStateMessageTx <- newStateMessage
+			broadcastStateMessageTx <- stateMessage
+            stateMessage.Sequence += 1
 			lastStateBroadcast = time.Now().UnixMilli()
 		}
 	}
