97dfa2aa3b23ad267e0956e526d09747c861c163
Vegar Karlsen <vegar@live.no>
2024-03-12 15:34:48 +0100

    Signal the distributor rutine when new hallreqeust gets thorugh.

--- distributor/hash.go
+++ distributor/hash.go
@@ -1,4 +1,4 @@
-package distribitor
+package distributor
 
 import (
 	"bytes"
--- distributor/requestHandler.go
+++ distributor/requestHandler.go
@@ -1,9 +1,9 @@
-package distribitor
+package distributor
 
 import (
 	"Driver-go/elevio"
 	"elevator/config"
-	"fmt"
+	// "fmt"
 	"log/slog"
 	"time"
 )
@@ -17,6 +17,7 @@ type HallRequestUpdate struct {
 	Sequence  int
 }
 
+// TODO: check sequence, checksum etc
 func validAck(message HallRequestUpdate) bool {
 	return true
 }
@@ -25,30 +26,34 @@ func waitForHallOrderConfirmation(
 	mainID string,
 	buttonEvent elevio.ButtonEvent,
 	ackChan <-chan string,
+    signalDistributor chan <- bool,
 ) {
 	acknowledgmentsNeeded := len(localPeerStates) - 1
 	countAck := 0
 	startTime := time.Now().UnixMilli()
 
-	fmt.Println("[waitForConfirmation]: Required acks: ", acknowledgmentsNeeded)
-	// TODO: check against the ids, instead of just count
+	slog.Info("[waitForConfirmation]: Required acks: ", acknowledgmentsNeeded)
+	// TODO: check against the ids? do we want to pass the ack if we get mulitple from same elevator?
 
 	for {
 		select {
         case ackID := <-ackChan:
 			countAck += 1
-            fmt.Println("[waitForConfirmation] got ack from ", ackID, "ack count: ", countAck) // FIXME:
+            slog.Info("[waitForConfirmation] got ack from ", ackID, "ack count: ", countAck) // FIXME:
 
 		default:
 			if countAck >= acknowledgmentsNeeded {
 				setHallRequest(buttonEvent.Floor, int(buttonEvent.Button), true) // FIXME: RaceCondition between syncronizer update?
-				slog.Info("[waitForConfirmation] order complete", getHallReqeusts())
+				slog.Info("[waitForConfirmation] order got through, updating hallRequests.", getHallReqeusts())
+
+                // send a signal to distributor that hallreqeusts is updated
+                signalDistributor <- true
 				// TODO: set on light here?
 				return
 			}
 			if time.Now().UnixMilli() >= startTime+config.HallOrderAcknowledgeTimeOut {
 				// Timeout, we drop the request.
-				fmt.Println("[waitForConfirmation]: timed out")
+				slog.Info("[waitForConfirmation]: timed out")
 				return
 			}
 		}
@@ -60,11 +65,12 @@ func RequestHandler(
 	broadcastRx <-chan HallRequestUpdate,
 	broadcastTx chan<- HallRequestUpdate,
 	buttonEvent <-chan elevio.ButtonEvent,
+    signalDistributor chan <- bool,
 ) {
 	newOrder := HallRequestUpdate{
 		Id:        mainID,
 		Checksum:  nil,
-		Sequence:  0, // TODO: use this
+		Sequence:  0,
 		Floor:     0,
 		Direction: 0,
 		Requestor: mainID,
@@ -91,12 +97,12 @@ func RequestHandler(
 				continue
 			}
 			slog.Info("[requestHandler] sending ack", slog.String("from", mainID), slog.String("To", incommingHallRequest.Requestor))
-			// ack the request
+            // ack the request
 			incommingHallRequest.Id = mainID
 			incommingHallRequest.Checksum, _ = HashStructSha1(incommingHallRequest)
 			broadcastTx <- incommingHallRequest
 
-        // TODO: skip if active ?
+        // TODO: skip if active (do we want to send out new request each time the button is pressed?)
 		case button := <-buttonEvent:
 			if button.Button == elevio.BT_Cab {
 				continue
@@ -106,9 +112,10 @@ func RequestHandler(
 			newOrder.Direction = int(button.Button)
 			newOrder.Checksum, _ = HashStructSha1(newOrder)
 			newOrder.Requestor = mainID
-			go waitForHallOrderConfirmation(mainID, button, acknowledgeGranted)
+			go waitForHallOrderConfirmation(mainID, button, acknowledgeGranted, signalDistributor)
 			broadcastTx <- newOrder
 			newOrder.Sequence += 1
 		}
+
 	}
 }
