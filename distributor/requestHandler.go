2f779b0175fc0ec494b70b5644675af5229a289c
Vegar Karlsen <vegar@live.no>
2024-03-14 01:10:11 +0100

    Formater.

--- distributor/requestHandler.go
+++ distributor/requestHandler.go
@@ -21,10 +21,9 @@ type HallRequestUpdate struct {
 }
 
 type HallOperation int
-
 const (
-    HRU_NONE HallOperation = 0
-	HRU_SET   HallOperation = 1
+	HRU_NONE  HallOperation = 0
+	HRU_SET                 = 1
 	HRU_CLEAR               = 2
 )
 
@@ -33,29 +32,28 @@ var (
 	hallRequestUpdateOverviewLock sync.Mutex
 )
 
-func getHallRequestUpdateMessage(id string)HallRequestUpdate{
-    hallRequestUpdateOverviewLock.Lock()
-    hallReq := hallRequestUpdateOverview[id]
-    hallRequestUpdateOverviewLock.Unlock()
-    return hallReq
+func getHallRequestUpdateMessage(id string) HallRequestUpdate {
+	hallRequestUpdateOverviewLock.Lock()
+	hallReq := hallRequestUpdateOverview[id]
+	hallRequestUpdateOverviewLock.Unlock()
+	return hallReq
 }
 
-func storeHallRequestUpdate(id string, request HallRequestUpdate){
-    hallRequestUpdateOverviewLock.Lock()
-    hallRequestUpdateOverview[id] = request
-    hallRequestUpdateOverviewLock.Unlock()
+func storeHallRequestUpdate(id string, request HallRequestUpdate) {
+	hallRequestUpdateOverviewLock.Lock()
+	hallRequestUpdateOverview[id] = request
+	hallRequestUpdateOverviewLock.Unlock()
 }
 
-// bad to update last pacakge? 
-func clearHallRequestUpdateOperationFlag(id string){
-    // TODO: check sequence here? what if the message is changed?
-    hallRequestUpdateOverviewLock.Lock()
-    current := hallRequestUpdateOverview[id]
-    current.Operation = HRU_NONE
-    hallRequestUpdateOverview[id] = current
-    hallRequestUpdateOverviewLock.Unlock()
+// bad to update last pacakge?
+func clearHallRequestUpdateOperationFlag(id string) {
+	// TODO: check sequence here? what if the message is changed?
+	hallRequestUpdateOverviewLock.Lock()
+	current := hallRequestUpdateOverview[id]
+	current.Operation = HRU_NONE
+	hallRequestUpdateOverview[id] = current
+	hallRequestUpdateOverviewLock.Unlock()
 }
-
 
 // var activeHallChanges map[string]HallRequestUpdate = make(map[string]HallRequestUpdate)
 
@@ -86,18 +84,18 @@ func clearHallRequestUpdateOperationFlag(id string){
 
 // TODO: check sequence, checksum etc
 func validAck(message HallRequestUpdate) bool {
+	checksum, _ := HashStructSha1(message)
+	if ValidateSha1Hash(checksum, message.Checksum) {
+		slog.Info("[validateAck] checksum does not match", "incomming.checksum", message.Checksum, "checksum", checksum)
+		return false
+	}
 
-    checksum, _ := HashStructSha1(message)
-    if ValidateSha1Hash(checksum, message.Checksum){
-        slog.Info("[validateAck] checksum does not match", "incomming.checksum", message.Checksum, "checksum", checksum)
-        return false
-    }
-    
 	return true
 }
 
 func waitForHallOrderConfirmation(
 	mainID string,
+	operation HallOperation,
 	buttonEvent elevio.ButtonEvent,
 	ackChan <-chan string,
 	signalDistributor chan<- bool,
@@ -108,6 +106,12 @@ func waitForHallOrderConfirmation(
 
 	slog.Info("[waitForConfirmation]: Required ", "acks", acknowledgmentsNeeded)
 	// TODO: check against the ids? do we want to pass the ack if we get mulitple from same elevator?
+    
+    // NOTE: should not be possible to start request if opreation == HRU_NONE
+    active := true
+    if operation == HRU_CLEAR{
+        active = false
+    }
 
 	for {
 		select {
@@ -117,11 +121,11 @@ func waitForHallOrderConfirmation(
 
 		default:
 			if countAck >= acknowledgmentsNeeded {
-				setHallRequest(buttonEvent.Floor, int(buttonEvent.Button), true)
+				setHallRequest(buttonEvent.Floor, int(buttonEvent.Button), active)
 				slog.Info("[waitForConfirmation]: order got through", "hallRequests", getHallReqeusts())
 
 				// send a signal to distributor that hallRequests is updated // TODO: move this?
-                // signalDistributor <- true // FIX:
+				// signalDistributor <- true // FIX:
 				// TODO: set on light here?
 				return
 			}
@@ -148,8 +152,7 @@ func RequestHandler(
 		Floor:     0,
 		Direction: 0,
 		Requestor: mainID,
-        Operation: HRU_NONE,
-
+		Operation: HRU_NONE,
 	}
 
 	acknowledgeGranted := make(chan string)
@@ -169,7 +172,7 @@ func RequestHandler(
 				if !validAck(incommingHallRequest) {
 					continue
 				}
-                // slog.Info("sending ack")
+				// slog.Info("sending ack")
 				acknowledgeGranted <- incommingHallRequest.Id
 				// slog.Info("[requestHandler] ack got", "From", incommingHallRequest.Id)
 				continue
@@ -182,15 +185,15 @@ func RequestHandler(
 			// 	continue
 			// }
 
-            if incommingHallRequest.Id != incommingHallRequest.Requestor{
-                slog.Info("[requestHanlder]: ignored ack", "from", incommingHallRequest.Id, "requester", incommingHallRequest.Requestor)
-                continue
-            }
+			if incommingHallRequest.Id != incommingHallRequest.Requestor {
+				slog.Info("[requestHanlder]: ignored ack", "from", incommingHallRequest.Id, "requester", incommingHallRequest.Requestor)
+				continue
+			}
 
-            storeHallRequestUpdate(incommingHallRequest.Requestor, incommingHallRequest)
+			storeHallRequestUpdate(incommingHallRequest.Requestor, incommingHallRequest)
 			slog.Info("[requestHanlder]: active request", "from", incommingHallRequest.Id, "requestor", incommingHallRequest.Requestor, "requestType", incommingHallRequest.Operation)
 
-            // acknowledge request.
+			// acknowledge request.
 			incommingHallRequest.Id = mainID
 			incommingHallRequest.Checksum, _ = HashStructSha1(incommingHallRequest)
 			broadcastTx <- incommingHallRequest
@@ -204,16 +207,30 @@ func RequestHandler(
 				continue
 			}
 
+			hallReq := getHallReqeusts()
+			if hallReq[button.Floor][button.Button] == true {
+				continue
+			}
+
 			slog.Info("[requestHanlder] ButtonPress registred", "floor", button.Floor, "dir", button.Button)
-            newOrder.Sequence += 1
+			newOrder.Sequence += 1
 			newOrder.Floor = button.Floor
 			newOrder.Direction = int(button.Button)
-			newOrder.Checksum, _ = HashStructSha1(newOrder)
 			newOrder.Requestor = mainID
-            newOrder.Operation = HRU_SET
+			newOrder.Operation = HRU_SET
+            newOrder.Checksum, _ = HashStructSha1(newOrder)
             storeHallRequestUpdate(mainID, newOrder)
-            go waitForHallOrderConfirmation(mainID, button, acknowledgeGranted, signalDistributor) // TODO: Should this be a a function instead of gorutine?
+			go waitForHallOrderConfirmation(mainID, newOrder.Operation, button, acknowledgeGranted, signalDistributor) // TODO: Should this be a a function instead of gorutine?
 			broadcastTx <- newOrder
+
+        // case clearRequest := <- clearHallRequestSignalChan:
+        //     newOrder.Sequence += 1
+        //     newOrder.Floor = 
+        //     newOrder.Direction 
+        //     newOrder.Requestor = mainID
+        //     newOrder.Operation = HRU_CLEAR
+        //     newOrder.Checksum, _ hashHashStructSha1(newOrder)
+        
 		}
 	}
 }
