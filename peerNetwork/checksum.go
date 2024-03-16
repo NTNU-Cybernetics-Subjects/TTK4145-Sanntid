1704e9cc08ab12bc7590ccdaa3d0ffac325e802b
Vegar Karlsen <vegar@live.no>
2024-03-16 02:01:05 +0100

    Move hash to peerNetwork, and rename to checksum.

---
+++ peerNetwork/checksum.go
@@ -0,0 +1,30 @@
+package peerNetwork
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/gob"
+)
+
+/*@param data: any
+*
+* This function can take any object as input parameter and returns
+* a sha1 hash of that object. If you use a struct as input parameter,
+* only the public variables will be considered. */
+func Checksum(data interface{}) ([]byte, error) {
+	var dataByteBuffer bytes.Buffer
+	encoder := gob.NewEncoder(&dataByteBuffer)
+
+	if err := encoder.Encode(data); err != nil {
+		return nil, err
+	}
+
+	hashSha1 := sha1.Sum(dataByteBuffer.Bytes())
+
+	return hashSha1[:4], nil
+}
+
+/* returns true if the byte array a i. */
+func ValidateChecksum(a []byte, b []byte) bool {
+	return bytes.Equal(a, b)
+}
